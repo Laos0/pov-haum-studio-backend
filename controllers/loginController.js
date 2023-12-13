bb3c52b1b8e0e0833839a136eb758e0931335eb1
laos0 <isonylao@gmail.com>
2023-12-13 14:10:17 -0600

    added in crypto and jwt libraries to generate json web token, users now get a token when they login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,8 @@
 /** This file deals with the logics of logging in */
-import pool from "../database.js"
+import pool from "../database.js";
 import { DatabaseEnums } from "../databaseEnums/databaseEnums.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto"
 
 const login = async (req, res) => {
     try{
@@ -14,8 +16,15 @@ const login = async (req, res) => {
         if(password === result[0].password){
 
             console.log("The password is: ", result[0].password);
-            res.status(200).json({ message: 'Logged in successfully' });
-            // TODO: do token here
+            
+            // generating secret key
+            const secretKey = crypto.randomBytes(32).toString('hex');
+            
+            // creating jwt
+            const token = jwt.sign({userEmail: email}, secretKey, {expiresIn: '30s'});
+
+            res.status(200).json({ message: 'Logged in successfully', token: token});
+
         }else{
             console.log("The password is: ", result[0].password);
             res.status(401).json({ message: 'Unauthorized: Incorrect password' });
