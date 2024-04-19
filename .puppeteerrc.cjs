d94b27c5489af31fea97808ba6c06fec5689e8c2
laos0 <isonylao@gmail.com>
2024-04-19 13:43:35 -0500

    reduced the size of chrome-puppeteer configs and introducing configs for puppeteer

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,22 @@
+// .puppeteer.config.js
+/*
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
+*/
+
+const {join} = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
